dbe0cc12f6c31a58d63c65529a0969f6e839d6dd
saikamal913 <saikamalvanaparthy.com>
2023-09-02 10:15:09 -0500

    changes text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,7 +29,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
+    const compiled =   fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('AngularPractice2 app is running!');
   });
 });
